81d5ab37d021ea0b752b5ccfef289eb2d16a2d7e
Hyemin <hyemin4246@gmail.com>
2021-06-03 15:52:25 +0900

    FBV로 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
+
     def __str__(self):
         return f'[{self.pk}]{self.title}' # pk field에 포스트의 제목과 번호를 문자열로 표현
 
+    def get_absolute_url(self): # 모델의 레코드별 url 생성 규칙
+        return f'blog/{self.pk}/' # blog 뒤에 post의 pk를 붙임
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views  # 현재 폴더에 있는 views.py를 사용할 수 있게 가져와랑
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page), # url이 blog/(int)이면 single_post_page() 함수를 실행
+    path('blog/<int:pk>/', views.single_post_page), # url이 blog/(int)이면 single_post_page() 함수를 실행
     path('', views.index)  # 입력된 url이 blog/ 뒤에 아무것도 없다면 임포트한 views.py에 정의되어 있는 index() 함수를 실행
 ]
