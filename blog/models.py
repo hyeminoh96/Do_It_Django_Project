9271cf8bd012eac87a3721159eea58be15f63c8c
Hyemin <hyemin4246@gmail.com>
2021-06-07 15:11:28 +0900

    포스트 상세 페이지에 대표 이미지 추가 및 파일 업로드 필드 생성

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%M/%d/', blank=True) # 이미지를 저장할 폴더의 경로 규칙
     # note.하위 폴더를 깊게 지정해야 탐색 시간 단축
     # note. blank=True : 해당 필드가 필수 항목은 아님
+    file_upload = models.FileField(upload_to='blog/files/%Y/%M/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
