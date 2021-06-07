69e4a77d12011ba4cf03883991563825756b0213
Hyemin <hyemin4246@gmail.com>
2021-06-07 16:23:34 +0900

    테스트 코드 작성 및 포스트 없는 경우 test

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,63 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
 class TestView(TestCase):
+    # testcase 내 기본적으로 설정되어야 하는 내용 정의
+    def setUp(self):
+        self.client = Client() # setUp 함수 내에 Client()를 사용하겠다는 내용
+
     def test_post_list(self):
-        self.assertEqual(2, 3) # chapter TDD. 2와 3이 같은지 테스트
+        # 1.1 포스트 목록 페이지 가져오기
+        # client라는 가상의 사용자가 웹에 '127.0.0.1:8000/blog/'를 입력하면 열리는 웹 페이지 정보를 response에 저장
+        response = self.client.get('/blog/')
+        # 1.2 페이지 로드
+        # 실패하면 404를 response, 성공하면 200을 response
+        self.assertEqual(response.status_code, 200)
+        # 1.3 페이지 타이틀 'Blog'인지
+        # parsing된 html의 title이 Blog인지 확인
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+        # 1.4 네비게이션 바 존재 여부
+        # soup에 담긴 요소 중 nav 요소만 navbar에 저장
+        navbar = soup.nav
+        # 1.5 Blog, About Me가 네비게이션 바에 있음
+        # navbar 요소 중, Blog와 About Me가 있는지 확인
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+        # 2.1 메인 영역에 게시물이 하나도 없다면
+        # 작성된 포스트가 0개인지 확인
+        self.assertEqual(Post.objects.count(), 0)
+        # 2.2 '아직 게시물이 없습니다.'라는 문구 출력
+        # id가 main_area인 요소를 찾아 main_area에 저장
+        # main_area가 해당 텍스트인지 확인
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
+
+        # 3.1 게시물이 2개 있다면
+        # 가상의 게시물 2개 생성
+        post_001 = Post.objects.create(
+            title = '첫 번째 포스트입니다.',
+            content = 'Hello World'
+        )
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='Goodbye World'
+        )
+        # 2개가 생성되었는지 확인
+        self.assertEqual(Post.objects.count(), 2)
+
+        # 3.2 포스트 목록 페이지를 새로고침했을 때
+        # 새로고침하기 위해 1.1~1.3을 반복
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        # 3.3 메인 영역에 포스트 2개의 타이틀이 존재
+        # 포스트 2개가 잘 생성되었는지 확인
+        main_are = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        # 3.4 '아직 게시물이 없습니다.'문구 삭제됨
+        # 게시물이 없다는 문구가 출력되지 않음을 확인
+        self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
+
