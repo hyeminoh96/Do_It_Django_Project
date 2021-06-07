097c484d8b7d593287844f7a27ff47abc1cc1b0f
Hyemin <hyemin4246@gmail.com>
2021-06-07 14:44:13 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%M/%d/', blank=True) # 이미지를 저장할 폴더의 경로 규칙
+    # note.하위 폴더를 깊게 지정해야 탐색 시간 단축
+    # note. blank=True : 해당 필드가 필수 항목은 아님
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('', include('single_pages.urls')) # 도메인 뒤에 아무것도 없는 경우, single_pages앱에서 처리
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
